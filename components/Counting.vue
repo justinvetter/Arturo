e3534619347a5cbf6dc32ab048fda91b565c4020
Ryan Rutkin <ryan@arturo.ai>
2019-05-13 15:33:15 -0500

    The Requests Served counter is now formatted with commas and is responsive to screen size.

--- components/Counting.vue
+++ components/Counting.vue
@@ -1,95 +1,192 @@
 <template>
     <section class="counting">
-      <h4>AI Insights delivered & counting </h4>
-      <h2 id="countUp">1234</h2>
+      <div id="countPhrase" >
+        <h4 id="countPhraseWrapper" >AI Insights delivered & counting</h4>
+      </div>
+      <div id="countUp">
+        <h2 id="countUpWrapper" > </h2>
+      </div>
     </section>
 </template>
 
 <script>
 export default {
     mounted() {
-    function animateValue(id, start, end, duration) {
-      var obj = document.getElementById(id);
-      var range = end - start;
-      var minTimer = 50;
-      var stepTime = Math.abs(Math.floor(duration / range));
-
-      stepTime = Math.max(stepTime, minTimer);
-
-      var startTime = new Date().getTime();
-      var endTime = startTime + duration;
-      var timer;
-
-      function run() {
-        var now = new Date().getTime();
-        var remaining = Math.max((endTime - now) / duration, 0);
-        var value = Math.round(end - remaining * range);
-        obj.innerHTML = value;
-        if (value == end) {
-          clearInterval(timer);
+      var canv = document.createElement('canvas');
+      var ctx = canv.getContext('2d');
+      var container = document.getElementById('countUpWrapper');
+      var previous_length = null;
+      var initial_font_size = 14.5; // vh
+      var size_tolerance_min = 0.2;
+      var size_tolerance_max = 0.12;
+      function fitTextToContainer(str) {
+        if (str.length === previous_length) {
+          return;
         }
+        previous_length = str.length;
+        
+        ctx.font = `${initial_font_size}vh "Gotham", Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif`;
+        var size = ctx.measureText(str).width;
+        var perc = initial_font_size;
+        var tolerance_min = container.getBoundingClientRect().width * (1 - size_tolerance_min);
+        var tolerance_max = container.getBoundingClientRect().width * (1 - size_tolerance_max);
+        if (size < tolerance_max) {
+          container.style.fontSize = `${initial_font_size}vh`;
+          return;
+        }
+        var sm_perc = null;
+        var lg_perc = initial_font_size;
+        var max_steps = 25;
+        var step = -1;
+        while (size < tolerance_min || size > tolerance_max) {
+          step++;
+          if (step >= max_steps) {
+            break;
+          }
+          if (size < tolerance_min) {
+            sm_perc = perc;
+          } else {
+            lg_perc = perc;
+          }
+          if (sm_perc === null) {
+            perc = lg_perc / 10;
+          } else {
+            perc = (lg_perc + sm_perc) / 2;
+          }
+          ctx.font = `${perc}vh "Gotham", Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif`;
+          size = ctx.measureText(str).width;
+        }
+        container.style.fontSize = `${perc}vh`;
       }
 
-      timer = setInterval(run, stepTime);
-      run();
-    }
+      function format_number(num) {
+        var p_num = parseInt(num);
+        if (isNaN(p_num) || !isFinite(p_num)) {
+          return String(num);
+        }
+        p_num = String(p_num);
+        var retval = '';
+        for (var i = p_num.length - 1; i >= 0; i--) {
+          var n_id = p_num.length - (i + 1);
+          if (retval.length && n_id % 3 === 0) {
+            retval = ',' + retval;
+          }
+          retval = p_num[i] + retval;
+        }
+        return retval;
+      }
+      
+      function animateValue(id, start, end, duration) {
+        var obj = document.getElementById(id);
+        var range = end - start;
+        var minTimer = 50;
+        var stepTime = Math.abs(Math.floor(duration / range));
 
-    animateValue("countUp", 0, 1234, 3000);
+        stepTime = Math.max(stepTime, minTimer);
 
+        var startTime = new Date().getTime();
+        var endTime = startTime + duration;
+        var timer;
+
+        function run() {
+          var now = new Date().getTime();
+          var remaining = Math.max((endTime - now) / duration, 0);
+          var value = Math.round(end - remaining * range);
+          obj.innerText = format_number(value);
+          fitTextToContainer(obj.innerText);
+          if (value == end) {
+            clearInterval(timer);
+          }
+        }
+
+        timer = setInterval(run, stepTime);
+        run();
+      }
+      animateValue("countUpWrapper", 0, 1234, 3000);
     }
 }
 </script>
 
 <style lang="scss" scoped>
     .counting {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 20vh;
-    padding-bottom: 5vh;
+      display: flex;
+      height: 25vh;
+      width: 100%;
+      padding: 5vh 2vh 5vh 70px; // sidemenu style
+      box-sizing: border-box;
 
-    h4 {
+      #countPhrase {
+        width: 38%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 3rem;
+        box-sizing: border-box;
+      }
+
+      #countPhraseWrapper {
         color: $color_darkBlue;
         font-weight: 100;
         font-size: em(24);
+        display: flex;
+        align-items: center;
+        justify-content: center;
         width: 125px;
+        flex: none;
         line-height: 1.25;
-    }
+        box-sizing: border-box;
+      }
 
-    h2 {
-        color: $color_blue;
+      #countUp {
+        position: relative;
         font-size: em(125.78);
-        font-weight: bold;
         border-left: solid 2px $color_teal;
-        line-height: 1;
-        margin-left: 3rem;
         padding-left: 3rem;
-        text-align: right;
-        min-width: 300px;
-    }
+        width: 62%;
+        height: 100%;
+        box-sizing: border-box;
+      }
 
-    @media (max-width: 825px) {
+      #countUpWrapper {
+        color: $color_blue;
+        font-weight: bold;
+        line-height: 1;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        box-sizing: border-box;
+        justify-content: center;
+      }
+
+      @media (max-width: 825px) {
         flex-direction: column;
         align-items: center;
         justify-content: center;
         margin-top: 1rem;
+        height: 20vh;
 
-        h4 {
-            width: 100%;
-            text-align: center;
-            padding-bottom: 1rem;
-            font-size: em(20);
+        #countPhrase {
+          width: 100%;
+          justify-content: center;
+          padding-bottom: 1rem;
+          font-size: em(20);
         }
 
-        h2 {
-            border-left: none;
-            border-top: solid 2px $color_teal;
-            text-align: center;
-            margin-left: 0;
-            padding-left: 0;
-            padding-top: .5rem;
-            font-size: em(100);
+        #countPhraseWrapper {
+          width: auto;
         }
+
+        #countUp {
+          width: 100%;
+          border-left: none;
+          border-top: solid 2px $color_teal;
+          margin-left: 0;
+          padding-left: 0;
+          padding-top: .5rem;
+          font-size: em(100);
+        }
+      }
     }
-}
 </style>

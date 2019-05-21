424e239b4b6e93e42c0425d0e3ec4e70133ef292
Ryan Rutkin <ryan@arturo.ai>
2019-05-21 15:05:30 -0500

    Added the thank-you page

---
+++ components/RequestADemo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="footer__top">
+        <div class="footer__top__card">
+            <div class="card-left">
+                <h3 class="h__dkBlue">Request a demo</h3>
+                <p class="t__dkBlue"
+                >Would you like to receive a demo of one of our amazing products? Click the link below to fill out a quick form, we promise we will not share any of your information we gather with third party companies.</p>
+                <a class="button" v-on:click="show">Get the demo</a>
+            </div>
+            <div class="vr"></div>
+            <div class="card-right">
+                <MediumIcon/>
+                <h5 class="h__dkBlue">Subscribe to our blog on Medium</h5>
+                <a class="button" href="https://medium.com/@arturo.ai" target="_blank">Visit Blog</a>
+            </div>
+        </div>
+        <Modal/>
+    </div>
+</template>
+
+<script>
+    import MediumIcon from "@/components/icons/Medium-icon/medium_icon_white.svg?inline";
+    import Modal from "@/components/Modal";
+
+    export default {
+    components: {
+        MediumIcon,
+        Modal
+    },
+    methods: {
+        show() {
+        this.$modal.show("demo");
+        }
+    }
+    };
+</script>
+
+<style>
+</style>
---
+++ pages/thank-you.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page" >
+    <ScrollForMore/>
+    <div class="page-body" >
+      <div class="thank-you-wrapper" >
+        <h2 class="thank-you-header" >
+            Thank you for requesting a demo from Arturo.
+        </h2>
+        <p class="thank-you-body" >
+            A representative from Arturo will be in contact with you soon.
+        </p>
+      </div>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import ScrollForMore from "@/components/ScrollForMore";
+import Footer from "@/layouts/partials/footer-wo-demo";
+export default {
+  components: {
+    ScrollForMore,
+    Footer
+  },
+  head() {
+    return {
+      title: "Thank You | Arturo",
+      meta: [{ hid: "description", name: "description", content: "Thank you for requesting a demo." }]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .page-body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .thank-you {
+    &-wrapper {
+      width: 70vw;
+      height: 50vh;
+      max-width:100%;
+      padding: 4vh;
+      background-color: $color_blue;
+      color: white;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      @media screen and (max-width:998px) {
+        width: 100%;
+      }
+    }
+
+    &-header {
+      font-size: 250%;
+      padding: 1vh 0;
+    }
+
+    &-body {
+      padding: 1vh 0;
+      font-size: 150%;
+    }
+  }
+</style>

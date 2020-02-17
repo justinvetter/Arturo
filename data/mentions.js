f6a00e303af9ab3b31b1cc39e5fa57f31bb0dc0b
Ben Richardson <benjamin.c.richardson.0@gmail.com>
2020-02-17 14:42:41 -0600

    Enforce source list.

--- data/mentions.js
+++ data/mentions.js
@@ -1,130 +1,146 @@
 
+export const sources = {
+    fortune: 'Fortune',
+    seraphim: 'Seraphim Capital',
+    americanInno: 'American Inno',
+    builtIn: 'Built In',
+    businessWire: 'Business Wire',
+    geospatialWorld: 'Geospatial World',
+    carrierManagement: 'Carrier Management',
+    aiAuthority: 'AI Authority',
+    digIn: 'Dig In',
+    claimsJournal: 'Claims Journal',
+    crowdfundInsider: 'Crowdfund Insider',
+    propertyCasualty360: 'Property Casualty 360',
+    prWeb: 'PRWeb'
+}
+
 
 const mediaMentions = [
     {
         date: 'February 4th, 2020',
         title: 'Labelbox Round Raised (jC Interviewed)',
-        sourceName: 'Fortune',
+        source: sources.fortune,
         link: "//fortune.com/2020/02/04/artificial-intelligence-data-labeling-labelbox/"
     },
     {
         date: 'February 3, 2020',
         title:'Space Tech Map 2020',
-        sourceName: 'Seraphim Capital',
+        source: sources.seraphim,
         link: '//seraphimcapital.passle.net/post/102fxy3/space-tech-map-2020-emerging-vc-backed-leaders'
     },
     {
         date: 'January 24, 2020',
         title:'Chicago Inno Start Ups to Watch Event',
-        sourceName: 'American Inno',
+        source: sources.americanInno,
         link: '//www.americaninno.com/chicago/state-of-innovation/2020-vision-what-you-missed-at-chicago-innos-startups-to-watch-event-photos/'
     },
     {
         date: 'January 21, 2020',
         title:'Startups to Watch 2020',
-        sourceName: 'Built In',
+        source: sources.builtIn,
         link: '//www.builtinchicago.org/2020/01/21/50-chicago-startups-watch-2020'
     },
     {
         date: 'December 19,2019',
         title:'Startups to Watch 2020',
-        sourceName: 'American Inno',
+        source: sources.americanInno,
         link: '//www.americaninno.com/chicago/20-Startups-to-Watch-in-2020-chi'
     },
     {
         date: 'September 23,2019',
         title:'Partnership with Branch',
-        sourceName: 'Business Wire',
+        source: sources.businessWire,
         link: '//www.businesswire.com/news/home/20190923005741/en/Arturo-Branch-Announce-Partnership-Integrate-Arturo%E2%80%99s-Deep'
     },
     {
         date: 'September 19,2019',
         title:'Neil Pearson joins team',
-        sourceName: 'Business Wire',
+        source: sources.businessWire,
         link: '//www.businesswire.com/news/home/20190919005647/en/Arturo-Announces-Addition-Leadership-Team-Neil-Pearson'
     },
     {
         date: 'September 18,2019',
         title:'Team up with GIC',
-        sourceName: 'Geospatial World',
+        source: sources.geospatialWorld,
         link: '//www.geospatialworld.net/news/geospatial-insurance-consortium-analytics-manage-risk/'
     },
     {
         date: 'September 11,2019',
         title:'Partnership with Hippo',
-        sourceName: 'PC360',
+        source: sources.digIn,
         link: '//www.dig-in.com/news/home-insurtech-hippo-partners-with-american-family-analytics-unit'
     },
     {
         date: 'August 23, 2019',
         title:'Tech Companies Redefining Insurance',
-        sourceName: 'Built In',
+        source: sources.builtIn,
         link: '//www.builtinchicago.org/2017/08/23/chicago-insurance-startups'
     },
     {
         date: 'August 13, 2019',
         title:'Partnership with Canopius',
-        sourceName: 'AI Authority',
+        source: sources.aiAuthority,
         link: '//www.aithority.com/technology/analytics/arturo-deep-learning-property-analytics-to-enhance-canopius-risk-selection/'
     },
     {
         date: 'August 12, 2019',
         title:'Partnership with Canopius',
-        sourceName: 'Business Wire',
+        source: sources.businessWire,
         link: '//www.businesswire.com/news/home/20190812005552/en/Arturo-deep-learning-property-analytics-enhance-Canopius-risk'
     },
     {
         date: 'July 29, 2019',
         title:'The Details on Arturo Q&A',
-        sourceName: 'Carrier Management',
+        source: sources.carrierManagement,
         link: '//www.carriermanagement.com/news/2019/07/29/195946.htm'
     },
     {
         date: 'July 29, 2019',
         title:'Partnership with Hippo',
-        sourceName: 'Claims Journal',
+        source: sources.claimsJournal,
         link: '//www.claimsjournal.com/news/national/2019/07/11/291904.htm'
     },
     {
         date: 'July 22, 2019',
         title:'Partnership with Hippo',
-        sourceName: 'Crowdfund Insider',
+        source: sources.crowdfundInsider,
         link: '//www.crowdfundinsider.com/2019/07/149749-arturo-announces-new-partnership-with-insurtech-hippo-insurance/'
     },
     {
         date: 'July 11, 2019',
         title:'Partnership with Hippo',
-        sourceName: 'Carrier Management',
+        source: sources.carrierManagement,
         link: '//www.carriermanagement.com/news/2019/07/11/195322.htm'
     },
     {
         date: 'July 11, 2019',
         title:'Partnership with Hippo',
-        sourceName: 'Claims Journal',
+        source: sources.claimsJournal,
         link: '//www.claimsjournal.com/news/national/2019/07/11/291904.htm'
     },
     {
         date: 'July 11, 2019',
         title:'Partnership with Hippo',
-        sourceName: 'PR Web',
+        source: sources.prWeb,
         link: '//www.prweb.com/releases/arturo_partners_with_hippo_insurance_to_provide_homeowners_better_pricing_underwriting_and_renewals/prweb16430015.htm'
     },
     {
         date: 'June 5, 2019',
         title:'Around the P&C Insurance Industry',
-        sourceName: 'PC360',
+        source: sources.propertyCasualty360,
         link: '//www.propertycasualty360.com/2019/06/05/around-the-pc-insurance-industry-june-5-2019/'
     },
     {
         date: 'May 28, 2019',
         title:'Spin out from AmFam',
-        sourceName: 'Chicago Inno',
+        source: sources.americanInno,
         link: '//www.americaninno.com/chicago/first-look/amfam-born-ai-startup-comes-out-of-stealth-mode-lands-seed-round/'
     },
     {
         date: 'May 28, 2019',
         title:'Spin out from AmFam',
-        sourceName: 'Business Wire',
+        source: sources.businessWire,
         link: '//www.businesswire.com/news/home/20190528005147/en/Arturo-Announces-Seed-Series-Close-Spin-out-American'
     },
 ]
